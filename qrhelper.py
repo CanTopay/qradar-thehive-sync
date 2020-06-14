11d121b1c86b0c0f7e5369f239d1be2eafa2b780
CanTopay <can.topay@gmail.com>
2020-06-14 21:40:56 +0200

    initial commit

---
+++ qrhelper.py
@@ -0,0 +1,245 @@
+import os
+import requests
+import json
+import time
+import logging
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+#Enable logging
+appname = os.path.basename(__file__)
+formatter = logging.Formatter('%(asctime)s %(name)s %(levelname)s %(message)s', "%b %d %H:%M:%S")
+logger = logging.getLogger(appname)
+logger.setLevel(logging.DEBUG)
+fh = logging.FileHandler('{}.log'.format(appname))
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+logger.addHandler(fh)
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+ch.setFormatter(formatter)
+logger.addHandler(ch)
+
+class qrhelper(object):
+    def __init__(self, qrurl, qrtoken, apiver, verify=False):
+        self.qrurl = qrurl
+        self.offenses = '/api/siem/offenses'
+        self.offense_types = '/api/siem/offense_types'
+        self.offense_closing_reasons = '/api/siem/offense_closing_reasons'
+        self.searches = '/api/ariel/searches'
+        self.reference_data = '/api/reference_data'
+        self.config = '/api/config'
+        self.log_sources = '/api/config/event_sources/log_source_management/log_sources'
+        self.rules = '/api/analytics/rules'
+        self.building_blocks = '/api/analytics/building_blocks'
+        self.source_addresses = '/api/siem/source_addresses/{}'
+        self.local_destination_addresses = '/api/siem/local_destination_addresses/{}'
+        self.headers = {"Accept": "application/json", "Content-Type": "application/json", "Version": apiver ,"SEC": qrtoken}
+        self.verify = verify
+        #Better to verify TLS in prd. => verify=True
+
+    def qr_get(self, endpoint_url):
+        result = None
+        try:
+            response = requests.get(url='{}{}'.format(self.qrurl, endpoint_url), headers=self.headers, verify=self.verify)
+            if response.status_code == 200:
+                resp_json = json.loads(response.content)
+                result = resp_json
+            else:
+                logger.error('Cannot get results.:{}-{}'.format(response.status_code,response.content))
+        except Exception as e:
+            logger.error('API Error. Failed GET:{}'.format(e))
+        return result
+    
+    def qr_post(self, endpoint_url, params = None, data=None):
+        result = None
+        #data = {"query_expression": qry_ex}
+        try:
+            response = requests.post(url='{}{}'.format(self.qrurl, endpoint_url), params=params, data=json.dumps(data), headers=self.headers, verify=False)
+            if response.status_code == 200 or response.status_code == 201:
+                logger.info('POST successful to endpoint:{}'.format(endpoint_url))
+                resp_json = json.loads(response.content)
+                result = resp_json
+            else:
+                logger.error('Cannot post data.:{}-{}'.format(response.status_code,response.content))
+        except Exception as e:
+            logger.error('API Error. Failed POST:{}'.format(e))
+        return result
+
+    def get_offenses(self, max_items=None, open=None):
+        ## https://server.com/api/siem/offenses?filter=status%3D%22OPEN%22
+        ##To limit max_items=99
+        if max_items:
+            self.headers.update({"Range": "items=0-%d" % max_items})
+        ##To filter Closed offenses open=True
+        if open is True:
+            filter = '?filter=status%3D%22OPEN%22'
+        else:
+            filter = ''
+        return qrhelper.qr_get(self, '{}{}'.format(self.offenses, filter))
+    
+    def get_offense_details(self, offenseid):
+        return qrhelper.qr_get(self,'{}/{}'.format(self.offenses, offenseid))
+
+    def get_offense_notes(self, offenseid):
+        return qrhelper.qr_get(self,'{}/{}/notes'.format(self.offenses, offenseid))
+
+    def get_source_addresses(self, sip_id):
+        return qrhelper.qr_get(self, '{}'.format(self.source_addresses.format(sip_id)))
+    
+    def get_local_destination_addresses(self, dip_id):
+        return qrhelper.qr_get(self, '{}'.format(self.local_destination_addresses.format(dip_id)))
+    
+    def get_rules(self, max_items=None):
+        ##To limit max_items=99
+        if max_items:
+            self.headers.update({"Range": "items=0-%d" % max_items})
+        return qrhelper.qr_get(self, '{}'.format(self.rules))
+    
+    def get_building_blocks(self, max_items=None):
+        ##To limit max_items=99
+        if max_items:
+            self.headers.update({"Range": "items=0-%d" % max_items})
+        return qrhelper.qr_get(self, '{}'.format(self.building_blocks))
+
+    def get_rule_name(self, rule_id):
+        return qrhelper.qr_get(self,'{}/{}'.format(self.rules, rule_id))
+
+    def get_offense_types(self):
+        return qrhelper.qr_get(self, '{}'.format(self.offense_types))
+    
+    def get_offense_type_name(self, type_id):
+        types = self.get_offense_types()
+        for i in types:
+            if type_id == i['id']:
+                index_name = i['name']
+                return index_name
+
+    def get_offense_type_property(self, type_id):
+        types = self.get_offense_types()
+        for i in types:
+            if type_id == i['id']:
+                index_name = i['property_name']
+                return index_name
+
+    def get_logsources(self, max_items=None, enabled=None):
+        ##https://server.com/api/siem/offenses?filter=status%3D%22OPEN%22
+        ##To filter disabled enabled=True.
+        ##To limit max_items=99
+        if max_items:
+            self.headers.update({"Range": "items=0-%d" % max_items})
+        if enabled is True:
+            filter = '?filter=enabled%3Dtrue'
+        else:
+            filter = ''
+        return qrhelper.qr_get(self, '{}{}'.format(self.log_sources, filter))
+    
+    def get_refset(self, refset):
+        return qrhelper.qr_get(self, '{}/sets/{}'.format(self.reference_data, refset))
+
+    def get_refmap(self, refmap):
+        # #./ReferenceDataUtil.sh create userDataMap MAP ALN
+        return qrhelper.qr_get(self, '{}/maps/{}'.format(self.reference_data, refmap))
+    
+    def get_reftable(self, reftable):
+        # #./ReferenceDataUtil.sh create UsrDataTable REFTABLE ALN -keyType=ip:IP, hostname:ALNIC, email:ALN, employeeid:ALN, vuln:ALN
+        return qrhelper.qr_get(self, '{}/tables/{}'.format(self.reference_data, reftable))
+
+    def post_refset(self, refset, value):
+        #Data format should match the data type of refset - IP, ALN, etc.
+        return qrhelper.qr_post(self, '{}/sets/{}?value={}'.format(self.reference_data, refset, value))
+
+    def post_refmap(self, refmap, json_data):
+        # #./ReferenceDataUtil.sh create userDataMap MAP ALN
+        # # Data should be in dict with key/value static mappings: {"key":"CAN","value":"192.168.1.1"}
+        return qrhelper.qr_post(self, '{}/maps/{}'.format(self.reference_data, refmap), params=json_data)
+
+    def post_bulkrefmap(self, refmap, json_data):
+        # # Data should be in Json/dict with unique keys and assigned value pairs: {"CAN":"192.168.1.1", "ADMIN":"192.168.1.1", "GUEST":"192.168.1.9"}
+        return qrhelper.qr_post(self, '{}/maps/bulk_load/{}'.format(self.reference_data, refmap), data=json_data)
+
+    def post_reftable(self, reftable, json_data):
+        # # Data should be in Json/dict with outer and inner key mappings and values: {"outer_key":"CAN", "inner_key": "ip", "value":"192.168.1.1"}
+        # # Dont forget key types while preparing data: UsrDataTable REFTABLE ALN -keyType=ip:IP, hostname:ALNIC, email:ALN, employeeid:ALN, vuln:ALN
+        return qrhelper.qr_post(self, '{}/tables/{}'.format(self.reference_data, reftable), params=json_data)
+
+    def post_bulkreftable(self, reftable, json_data):
+        # # Data should be in Json/dict with outer/inner keys and values: {"ADMIN":{"ip":"192.168.1.1", "hostname":"ADMINSYS", "email":"admin@admin.com","employeeid":"999","vuln":"INFO-99"}}
+        # # Dont forget key types while preparing data: UsrDataTable REFTABLE ALN -keyType=ip:IP, hostname:ALNIC, email:ALN, employeeid:ALN, vuln:ALN
+        return qrhelper.qr_post(self, '{}/tables/bulk_load/{}'.format(self.reference_data, reftable), data=json_data)
+
+    def post_offense_note(self, offenseid, note_text):
+        return qrhelper.qr_post(self,'{}/{}/notes?note_text={}'.format(self.offenses, offenseid, note_text))
+    
+    def post_aql(self, qry_ex):
+        search_id = None
+        qry = {'query_expression': qry_ex}
+        search_id = qrhelper.qr_post(self, '{}'.format(self.searches), params=qry)['search_id']
+        if search_id:
+            logger.info('AQL post successful.search_id:{}'.format(search_id))
+        return search_id
+
+    def get_aql_results(self, search_id):
+        results = None
+        qry = qrhelper.qr_get(self, '{}/{}'.format(self.searches, search_id))
+        if qry and qry['status'] == 'COMPLETED':
+            results = qrhelper.qr_get(self, '{}/{}/results'.format(self.searches, search_id))
+        else:
+            pass
+        return results
+
+    def run_aql(self, qry_ex):
+    #NOTE: run_aql() can take long time and might hammer the system if you have many results or long timeframes.
+    # #Therefore I limit execs up to 10 and put a sleep for 1 secs between them.
+    # #If you need more/longer time frames, either change timer or use above post and later get queries using saved search_id's.
+        results = None
+        data = {'query_expression': qry_ex}
+        search_id = qrhelper.post_aql(self, qry_ex)
+        if search_id:
+            timer = 0
+            results = qrhelper.get_aql_results(self, search_id)
+            while results == None and timer < 10:
+                time.sleep(1)
+                results = qrhelper.get_aql_results(self, search_id)
+                timer += 1
+        return results
+
+    def close_offense(self, offense_id, closing_reason_text):
+        closed = False
+        response = qrhelper.qr_get(self, '{}?filter=text%3D%22{}%22'.format(self.offense_closing_reasons, closing_reason_text))
+        if response:
+            id = response[0]['id']
+            resp_close = qrhelper.qr_post(self, '{}/{}?closing_reason_id={}&status=CLOSED'.format(self.offenses, offense_id, id))
+            if resp_close and resp_close['status'] == 'CLOSED':
+                logger.info('Offense Closed.Offense ID:{}'.format(offense_id))
+                closed = True
+            else:
+                logger.error('Cannot close offense.Offense ID:{}'.format(offense_id))
+        else:
+            logger.error('Çannot find Closing Reason ID. Check logs and closing reason text:{}'.format(closing_reason_text))
+        return closed
+
+
+
+# #Exp Usage:
+
+# a = qrhelper('https://192.168.1.1','token-xxxx-xxxx-xxxx-xxxxxxxxxxx','12.0')
+
+# print(a.post_refset('Critical Assets','192.168.199.199'))
+
+# json_data = {"key":"CAN","value":"192.168.1.1"}
+# print(a.post_refmap('userDataMap', json_data))
+
+# json_data = {"CAN":"192.168.1.1", "ADMIN":"192.168.1.1", "GUEST":"192.168.1.9"}
+# print(a.post_bulkrefmap('userDataMap', json_data))
+
+# json_data = {"outer_key":"CAN", "inner_key": "ip", "value":"192.168.1.1"}
+# print(a.post_reftable('UsrDataTable', json_data))
+
+# json_data = {"ADMIN":{"ip":"192.168.1.1", "hostname":"ADMINSYS", "email":"admin@admin.com","employeeid":"999","vuln":"INFO-99"}}
+# print(a.post_bulkreftable('UsrDataTable', json_data))
+
+# qry = "SELECT * FROM events START '2020-06-10 10:00' STOP '2020-06-10 13:00'"
+# print(a.run_aql(qry))
+
+# a.close_offense(999,'Non-Issue')
---
+++ sync-offense.py
@@ -0,0 +1,296 @@
+####by CanT####
+####v.2####
+from os import sys, path
+sys.path.append(path.dirname(path.dirname(path.abspath(__file__))))
+from settings.settings import *
+from lib.qradar_helper import qradar
+from lib.sqlite_helper import sqlite_helper
+from thehive4py.api import TheHiveApi
+from thehive4py.models import Case, CustomFieldHelper, CaseTask, CaseObservable
+
+from lib.logger import logger
+lgr = logger('sync-offense', syslog_logging_server)
+
+import urllib3
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+# # Check offense desc and filter out test offenses using prefix Dummy--xxx--.
+def check_test_offenses(offense_desc):
+    test_offense = False
+    for i in test_offense_descs:
+        if i.lower() in offense_desc.lower()[:len(i)]:
+            test_offense = True
+    return test_offense
+
+# #Check offense fields -- which we defined in settings.py -- to map observable types
+def offense_observable_mapping(obs_type, obs_src):
+    obs_dict = {}
+    for k,v in qradar_observable_mapping.items():
+        if obs_type.lower() == k.lower():
+            obs_dict[v] = obs_src
+    return obs_dict
+
+# #AQL results parsing
+def parse_get_aql(get_aql_results, target_item):
+    values_list = []
+    for i in get_aql_results['events']:
+        for k,v in i.items():
+            if k == target_item:
+                if v != None:
+                    values_list.append(v)
+    return values_list
+
+# #Create observables
+def create_observables(items_dict, case_id):
+    for k, v in items_dict.items():
+        observable = CaseObservable(dataType=k, data=[v], ioc=True, )
+        try:
+            resp = hive_api.create_case_observable(case_id, observable)
+            if resp.status_code == 201:
+                id = resp.json()['id']
+                return id
+        except Exception as err:
+            lgr.error('Error at observable creation.Case Id::{} - Err:[]'.format(case_id, err))
+            sys.exit(1)
+
+# #Prepare AQL query for enrichments
+def aql_enrich_qry(field, offense_id):
+    aql_qry = "SELECT \"{}\" FROM events WHERE inOffense({}) GROUP BY \"{}\" LIMIT {} START '{}' STOP '{}'"\
+            .format(field, offense_id, field, max_event_count, offense_start_time, offense_lastupdate_time)
+    return aql_qry
+
+# # A dummy sev,tlp assignment for initial case creation - using ONLY offense magnitude.
+# severity: (0: not set; 1: low; 2: medium; 3: high)
+# TLP: (-1: unknown; 0: white; 1: green; 2: amber; 3: red)
+#TODO: Triage-assignment severity calculation.
+def offense_severity_mapper(magnitude):
+    mapper = {}
+    sev = 0
+    tlp = 0
+    if magnitude <= 2:
+        sev = 1
+        tlp = 0
+    elif magnitude >= 3 and magnitude <= 6:
+        sev = 2
+        tlp = 1
+    elif magnitude >= 7 and magnitude < 9:
+        sev = 3
+        tlp = 2
+    elif magnitude > 9:
+        sev = 3
+        tlp = 3
+    mapper['sev'] = sev
+    mapper['tlp'] = tlp
+    return mapper
+
+# # Connect to DB, or create a new one.
+lgr.info('Connecting to Sqlite DB:{} in {}'.format(case_db, db_path))
+sl = sqlite_helper()
+sl.create_case_table()
+sl.create_enrichment_table()
+
+# #Connect to TheHive
+try:
+    hive_api = TheHiveApi(thehive_url, thehive_token, cert=False)
+    lgr.info('Connected to TheHive:{}'.format(thehive_url))
+except Exception as e:
+    lgr.error('Error at connecting to TheHive:{} Error:{}'.format(thehive_url, e))
+    sys.exit(1)
+
+# # Query Qradar for open Offenses
+lgr.info('Connecting to Qradar:{}'.format(qr_url))
+qr = qradar()
+# #Check for already closed cases
+get_cases = sl.get_records_by_field(case_table, 'case_id', 'status', 'Open')
+for i in get_cases:
+    #print(hive_api.get_case('qt-0wHABMIVxRx-xHkPO').json())
+    if hive_api.get_case(i[0]).json()['status'] == 'Resolved':
+        for i in sl.get_records_by_field(case_table, 'id', 'case_id', i[0]):
+            if qr.get_offense_details(i[0])['status'] == 'OPEN':
+                qr.close_offense(i[0], offense_closing_reason_id)
+                sl.update_record(case_table, 'status', 'Resolved', 'id', i[0])
+                sl.update_record(enrichment_table, 'status', 'Closed', 'id', i[0])
+
+# #Process new offenses
+qr_resp = qr.get_offenses(open=True, max_items=max_event_count)
+new_offense_list = []
+if qr_resp is not None:
+    for i in qr_resp:
+        # Check offense_start_from ID-number and process only bigger
+        if i['id'] > offense_start_from:
+            # Check offense description(name) for test, dummy offenses
+            if check_test_offenses(i['description'].strip()) == False:
+                # Check sqlite db for past synced offenses
+                if sl.check_synced(case_table, 'id', i['id']) is not True:
+                    # New offenses to process
+                    new_offense_list.append(i['id'])
+if len(new_offense_list) == 0 :
+    lgr.info('No new offense to sync right now. All good!')
+
+# #Create new cases from offenses
+qr = qradar()
+for i in new_offense_list:
+    print('-------Processing new offense-------:', i)
+    # # Getting values
+    offense_details = qr.get_offense_details(i)
+    offense_id = offense_details['id']
+    offense_desc = offense_details['description'].strip().replace('\n','')
+    offense_type_name = qr.check_offense_type_name_in_offense(offense_details['offense_type'])
+    offense_type_property = qr.check_offense_type_property_in_offense(offense_details['offense_type'])
+    offense_src_nw = offense_details['source_network']
+    offense_dst_nw = offense_details['destination_networks'][0]
+    offense_magnitude = offense_details['magnitude']
+    offense_start_time = offense_details['start_time']
+    offense_lastupdate_time = offense_details['last_updated_time']
+    offense_source = offense_details['offense_source']
+    offense_link = qr_url + 'console/qradar/jsp/QRadar.jsp?appName=Sem&pageId=OffenseSummary&summaryId={}'.format(offense_id)
+    #offense_rules = offense_details['rules']
+
+    # #Getting some more static offense data to be enriched
+    source_count = offense_details['source_count']
+    source_address_ids = offense_details['source_address_ids']
+    local_destination_count = offense_details['local_destination_count']
+    local_destination_addresses_ids = offense_details['local_destination_address_ids']
+    remote_destination_count = offense_details['remote_destination_count']
+    username_count = offense_details['username_count']
+
+    observables_dict = {}
+    # #SIP list from static Offense data
+    source_address_list = []
+    if source_count >= 1:
+        for index, x in zip(range(max_event_count), source_address_ids):
+            if x is not None:
+                sip = qr.get_source_addresses(x)
+                observables_dict['ip'] = sip
+                source_address_list.append(sip)
+
+    # #Local DIP list from static Offense data
+    local_dest_list = []
+    if local_destination_count >= 1:
+        for index, x in zip(range(max_event_count), local_destination_addresses_ids):
+            if x is not None:
+                dip = qr.get_local_destination_addresses(x)
+                observables_dict['ip'] = dip
+                local_dest_list.append(dip)
+
+    # #Remote DIP list from static Offense data
+    remote_dest_list = []
+    if remote_destination_count >= 1:
+        aql_id = qr.post_aql(aql_enrich_qry('destinationip', offense_id))
+        if aql_id:
+            sl.new_enrichment_record(offense_id, aql_id, 'destinationip', 'Open')
+            records = qr.get_aql_results(aql_id)
+            if records:
+                retval = parse_get_aql(records, 'destinationip')
+                if retval:
+                    for item in retval:
+                        if item != None:
+                            observables_dict['ip'] = item
+                            remote_dest_list.append(item)
+                            sl.update_record(enrichment_table, 'status', 'Closed', 'enrichment_id', aql_id)
+
+    # #Username list from static Offense data - don't post this as an observable
+    username_list = []
+    if username_count >= 1:
+        aql_id = qr.post_aql(aql_enrich_qry('username', offense_id))
+        if aql_id:
+            sl.new_enrichment_record(offense_id, aql_id, 'username', 'Open')
+            records = qr.get_aql_results(aql_id)
+            if records:
+                retval = parse_get_aql(records, 'username')
+                print('retval', retval)
+                if retval:
+                    for item in retval:
+                        if item != None:
+                            username_list.append(item)
+                            sl.update_record(enrichment_table, 'status', 'Closed', 'enrichment_id', aql_id)
+
+    # #More static fields to create case - ALL FIELDS MUST BE CREATED ON THE HIVE with SAME REFERENCE NAME.
+    custom_fields = CustomFieldHelper()
+    custom_fields.add_number('offense_id', offense_id)
+    custom_fields.add_string('offenseType', offense_type_name)
+    custom_fields.add_string('offense_source', offense_source)
+    custom_fields.add_number('offenseMagnitude',offense_magnitude)
+    custom_fields.add_date('offenseStartTime',offense_start_time)
+    custom_fields.add_date('offenseLastUpdate',offense_lastupdate_time)
+    custom_fields.build()
+    tlp = offense_severity_mapper(offense_magnitude)['sev']
+
+    # #Markdown offense summary
+    build_desc = """|Offense Summary:|\n|---|\n|Offense Description: {}|\n|Source NW: {}|\n|Destination NW: {}|\n|Source IPs: {}|\n|Local Destination IPs: {}|\n|Remote Destination IPs: {}|\n|Usernames: {}|\n---\nLink to the Offense: {}""".format(offense_desc, offense_src_nw, offense_dst_nw, source_address_list,
+                                                       local_dest_list, remote_dest_list, username_list, offense_link)
+
+    # #Some sample tasks-response actions for posting in the case. Customize per your reqs.
+    # #TODO: Dynamic task-playbook assignment
+    tasks = [CaseTask(title='PB:Malware response - Phase:Identification'), CaseTask(title='PB:Malware response - Phase:Remediation'), CaseTask(title='PB:Malware response - Phase:Lessons Learned', status='Waiting', flag=True)]
+
+    #Build TheHive Case with custom fields
+    thehive_case = Case(title=offense_desc,
+                        tlp=tlp,
+                        flag=True,
+                        tags=['offense', 'qradar', offense_type_name],
+                        description=build_desc,
+                        customFields=custom_fields.fields,
+                        tasks=tasks)
+
+    print('-------Posting Case with initial values--------')
+    case_id = None
+    try:
+        resp = hive_api.create_case(thehive_case)
+        if resp.status_code == 201:
+            case_id = resp.json()['id']
+            case_num = resp.json()['caseId']
+            qr.post_offense_note(offense_id, offense_note.format(case_num))
+            sl.new_case_record(offense_id, case_id, 'Open')
+            lgr.info('Case created. Case Id:{} - Case Num:{}'.format(case_id,case_num))
+    except Exception as err:
+        lgr.error('Error at case creation.:{}'.format(err))
+        sys.exit(1)
+
+    if case_id:
+        print('-------Posting Observables--------')
+        if offense_source not in list(observables_dict.values()):
+            # Offense source type and val for observables
+            off_source_mapping = offense_observable_mapping(offense_type_property, offense_source)
+            if off_source_mapping: # {'ip': '8.8.8.8'}
+                obs_id = create_observables(off_source_mapping, case_id)
+                lgr.info('Observable created. Case Id:{} - Obsv.Id:{}'.format(case_id, obs_id))
+        # Post-Get custom property based obs which we defined in qradar_observable_mapping dict.
+        for k, v in qradar_observable_mapping.items():
+            if k.lower() != offense_type_property.lower(): ##Offense source already added
+                aql_id = qr.post_aql(aql_enrich_qry(k,offense_id))
+                if aql_id:
+                    sl.new_enrichment_record(offense_id, aql_id, k, 'Open')
+                    records = qr.get_aql_results(aql_id)
+                    if records:
+                        retval = parse_get_aql(records, k)
+                        if retval:
+                            for item in retval:
+                                if item != None:
+                                    custom_prop_mapping = offense_observable_mapping(k, item)
+                                    obs_id = create_observables(custom_prop_mapping, case_id)
+                                    sl.update_record(enrichment_table, 'status', 'Closed', 'enrichment_id', aql_id)
+                                    lgr.info('Observable created. Case Id:{} - Obsv.Id:{}'.format(case_id,obs_id))
+
+# #Check for unfinished AQL queries from Sqlite for previous case enrichments.
+# #Close the enrichment records after update or result set==None, which means property does not in relation with the offense.
+chk_aql_dict = {}
+open_enrichments = sl.run_sql("SELECT c.case_id, e.enrichment_id from cases c LEFT OUTER JOIN enrichments e ON c.id = e.id where e.status = 'Open'")
+if len(open_enrichments) > 0:
+    print('-------Checking for past enrichment results--------')
+    for i in open_enrichments:
+        chk_aql_dict[i[0]] = i[1]
+        for k,v  in chk_aql_dict.items():
+            records = qr.get_aql_results(v)
+            if records:
+                obs_type = sl.get_records_by_field(enrichment_table, 'enrichment_type', 'enrichment_id', v)[0][0]
+                retval = parse_get_aql(records, obs_type)
+                if retval:
+                    for item in retval:
+                        if item != None:
+                            custom_prop_mapping = offense_observable_mapping(obs_type, item[0])
+                            obs_id = create_observables(custom_prop_mapping, k)
+                sl.update_record(enrichment_table, 'status', 'Closed', 'enrichment_id', v)
+                lgr.info('Past enrichment checks finished.')
+else :
+    lgr.info('No new enrichment to check right now. All good!')
